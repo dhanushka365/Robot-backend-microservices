b1555806e4b805065a74a7606195c301446e7d70
dhanushka365 <technohub10000.com>
2022-10-08 11:50:28 +0530

    Initial commit

---
+++ src/test/java/com/sjpfypgrp7/Robotbackendmicroservices/RobotBackendMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjpfypgrp7.Robotbackendmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotBackendMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
